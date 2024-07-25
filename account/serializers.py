2d1a6ae828c16ae4a79a6f95972afd2420a96478
HashirSaikhani <hashirsaikhani2018@gmail.com>
2024-07-25 19:14:39 +0500

    Added comments

--- account/serializers.py
+++ account/serializers.py
@@ -5,10 +5,11 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from account.utils import Util
 from django.contrib.auth import authenticate
- 
+
+# Serializer for user registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-    
+
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'address', 'phone', 'age', 'password', 'password2']
@@ -16,6 +17,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
+    # Validate that password and password2 match
     def validate(self, attrs):
         password = attrs.get('password')
         password2 = attrs.get('password2')
@@ -23,18 +25,20 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Password and Confirm Password do not match")
         return attrs
 
+    # Create a new user and set the password
     def create(self, validated_data):
-        password = validated_data.pop('password2')  
-        user = User.objects.create_user(**validated_data)  
-        user.set_password(password) 
+        password = validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
         user.save()
         return user
 
-
+# Serializer for user login
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
+    # Validate user credentials
     def validate(self, attrs):
         email = attrs.get('email')
         password = attrs.get('password')
@@ -43,95 +47,101 @@ class UserLoginSerializer(serializers.Serializer):
             raise serializers.ValidationError('Invalid email or password')
         attrs['user'] = user
         return attrs
-    
-    
+
+# Serializer for uploading files
 class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ['file', 'name']
     
+    # Create a new file instance
     def create(self, validated_data):
         return File.objects.create(**validated_data)
 
-
-class FileSerializer(serializers.ModelSerializer):
+# Serializer for listing files
+class FileListSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ['id', 'file', 'name', 'uploaded_at']
         read_only_fields = ['user', 'uploaded_at']
 
-      
-
+# Serializer for user profile
 class UserProfileSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ['id', 'email', 'name']
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'address', 'phone', 'age', 'no_of_files_uploaded']
 
+# Serializer for changing user password
 class UserChangePasswordSerializer(serializers.Serializer):
-  password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-  password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-  class Meta:
-    fields = ['password', 'password2']
+    password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
 
-  def validate(self, attrs):
-    password = attrs.get('password')
-    password2 = attrs.get('password2')
-    user = self.context.get('user')
-    if password != password2:
-      raise serializers.ValidationError("Password and Confirm Password doesn't match")
-    user.set_password(password)
-    user.save()
-    return attrs
+    class Meta:
+        fields = ['password', 'password2']
 
+    # Validate that password and password2 match
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        user = self.context.get('user')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doesn't match")
+        user.set_password(password)
+        user.save()
+        return attrs
+
+# Serializer for sending password reset email
 class SendPasswordResetEmailSerializer(serializers.Serializer):
-  email = serializers.EmailField(max_length=255)
-  class Meta:
-    fields = ['email']
+    email = serializers.EmailField(max_length=255)
 
-  def validate(self, attrs):
-    email = attrs.get('email')
-    if User.objects.filter(email=email).exists():
-      user = User.objects.get(email = email)
-      uid = urlsafe_base64_encode(force_bytes(user.id))
-      print('Encoded UID', uid)
-      token = PasswordResetTokenGenerator().make_token(user)
-      print('Password Reset Token', token)
-      link = 'http://localhost:3000/api/user/reset/'+uid+'/'+token
-      print('Password Reset Link', link)
-      # Send EMail
-      body = 'Click Following Link to Reset Your Password '+link
-      data = {
-        'subject':'Reset Your Password',
-        'body':body,
-        'to_email':user.email
-      }
-      Util.send_email(data)
-      return attrs
-    else:
-      raise serializers.ValidationError('You are not a Registered User')
+    class Meta:
+        fields = ['email']
 
+    # Validate the email and send password reset link if the user exists
+    def validate(self, attrs):
+        email = attrs.get('email')
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            uid = urlsafe_base64_encode(force_bytes(user.id))
+            token = PasswordResetTokenGenerator().make_token(user)
+            link = 'http://localhost:3000/api/user/reset/' + uid + '/' + token
+
+            # Send email with the reset link
+            body = 'Click Following Link to Reset Your Password ' + link
+            data = {
+                'subject': 'Reset Your Password',
+                'body': body,
+                'to_email': user.email
+            }
+            Util.send_email(data)
+            return attrs
+        else:
+            raise serializers.ValidationError('You are not a Registered User')
+
+# Serializer for resetting user password
 class UserPasswordResetSerializer(serializers.Serializer):
-  password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-  password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-  class Meta:
-    fields = ['password', 'password2']
+    password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
 
-  def validate(self, attrs):
-    try:
-      password = attrs.get('password')
-      password2 = attrs.get('password2')
-      uid = self.context.get('uid')
-      token = self.context.get('token')
-      if password != password2:
-        raise serializers.ValidationError("Password and Confirm Password doesn't match")
-      id = smart_str(urlsafe_base64_decode(uid))
-      user = User.objects.get(id=id)
-      if not PasswordResetTokenGenerator().check_token(user, token):
-        raise serializers.ValidationError('Token is not Valid or Expired')
-      user.set_password(password)
-      user.save()
-      return attrs
-    except DjangoUnicodeDecodeError as identifier:
-      PasswordResetTokenGenerator().check_token(user, token)
-      raise serializers.ValidationError('Token is not Valid or Expired')
-  
+    class Meta:
+        fields = ['password', 'password2']
+
+    # Validate the token and reset the password
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            password2 = attrs.get('password2')
+            uid = self.context.get('uid')
+            token = self.context.get('token')
+            if password != password2:
+                raise serializers.ValidationError("Password and Confirm Password doesn't match")
+            id = smart_str(urlsafe_base64_decode(uid))
+            user = User.objects.get(id=id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise serializers.ValidationError('Token is not Valid or Expired')
+            user.set_password(password)
+            user.save()
+            return attrs
+        except DjangoUnicodeDecodeError as identifier:
+            PasswordResetTokenGenerator().check_token(user, token)
+            raise serializers.ValidationError('Token is not Valid or Expired')
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer, SendPasswordResetEmailSerializer, UserPasswordResetSerializer, FileSerializer
+from account.serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer, SendPasswordResetEmailSerializer, UserPasswordResetSerializer, FileListSerializer
 from account.renderers import UserRenderer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
@@ -10,19 +10,20 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.core.files.storage import default_storage
 from rest_framework.pagination import PageNumberPagination
+import logging, os
 
-
-# Generate Token Manually
+# Function to generate JWT tokens for the user
 def get_tokens_for_user(user):
-  refresh = RefreshToken.for_user(user)
-  return {
-      'refresh': str(refresh),
-      'access': str(refresh.access_token),
-  }
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistrationView(APIView):
     renderer_classes = [UserRenderer]
 
+    # Handle user registration
     def post(self, request, format=None):
         serializer = UserRegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -30,10 +31,10 @@ class UserRegistrationView(APIView):
         token = get_tokens_for_user(user)
         return Response({'token': token, 'msg': 'Registration Successful'}, status=status.HTTP_201_CREATED)
 
-
 class UserLoginView(APIView):
     renderer_classes = [UserRenderer]
 
+    # Handle user login
     def post(self, request, format=None):
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -41,16 +42,15 @@ class UserLoginView(APIView):
         token = get_tokens_for_user(user)
         return Response({'token': token, 'msg': 'Login Success'}, status=status.HTTP_200_OK)
 
-
-
 class FileUploadView(APIView):
     permission_classes = [IsAuthenticated]
 
+    # Handle file upload
     def post(self, request, format=None):
         user = request.user
         files = request.FILES.getlist('file')
         if files:
-            total_uploaded = File.objects.filter(user=user).count()
+            total_uploaded = user.no_of_files_uploaded
             remaining_slots = 20 - total_uploaded
 
             if len(files) > remaining_slots:
@@ -62,32 +62,42 @@ class FileUploadView(APIView):
             for file in files:
                 File.objects.create(file=file, name=file.name, user=user)
 
+            # Update the count of files uploaded by the user
+            user.no_of_files_uploaded += len(files)
+            user.save()
+
             return Response({'message': 'Files uploaded successfully.'}, status=status.HTTP_201_CREATED)
         return Response({'error': 'No files uploaded.'}, status=status.HTTP_400_BAD_REQUEST)
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
+class FileListPagination(PageNumberPagination):
+    page_size = 15
 
 class FileListView(APIView):
     permission_classes = [IsAuthenticated]
+    pagination_class = FileListPagination
 
+    # List files with pagination
     def get(self, request, format=None):
         user = request.user
         files = File.objects.filter(user=user)
         
-        paginator = PageNumberPagination()
-        paginator.page_size = 15 
-        
+        paginator = FileListPagination()
         result_page = paginator.paginate_queryset(files, request)
-        serializer = FileSerializer(result_page, many=True)
+
+        logging.debug(f"Page number: {request.query_params.get('page', '1')}")
+        logging.debug(f"Number of files retrieved: {len(result_page)}")
+
+        serializer = FileListSerializer(result_page, many=True)
       
         return paginator.get_paginated_response(serializer.data)
-
-
-
 
 class FileView(APIView):
     permission_classes = [IsAuthenticated]
 
+    # View a file by its ID
     def get(self, request, file_id, format=None):
         file_instance = get_object_or_404(File, id=file_id, user=request.user)
 
@@ -100,66 +110,102 @@ class FileView(APIView):
             response['Content-Disposition'] = f'attachment; filename="{file_instance.name}"'
             return response
 
+    # Determine the content type of a file
     def get_content_type(self, filename):
         import mimetypes
         mime_type, _ = mimetypes.guess_type(filename)
         return mime_type or 'application/octet-stream'
 
-
-
-
 class FileDelete(APIView):
     permission_classes = [IsAuthenticated]
 
+    # Delete a file and update the user's file count
     def delete(self, request, file_id, format=None):
         file_instance = get_object_or_404(File, id=file_id, user=request.user)
-        file_instance.file.delete(save=False)
-        file_instance.delete()
-        return Response({'message': 'File deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
-    
+        user = request.user
 
+        # Delete the file from storage
+        file_instance.file.delete(save=False)
+        
+        # Delete the file instance
+        file_instance.delete()
+
+        # Decrement the user's file count and save the user instance
+        user.no_of_files_uploaded = max(0, user.no_of_files_uploaded - 1)
+        user.save()
+
+        return Response({'message': 'File deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
 class FileUpdateView(APIView):
     permission_classes = [IsAuthenticated]
 
+    # Update file details or content
     def put(self, request, file_id, format=None):
         user = request.user
         file_instance = get_object_or_404(File, id=file_id, user=user)
-        serializer = FileSerializer(file_instance, data=request.data, partial=True)
+        
+        # Extract data from the request
+        new_name = request.data.get('name', file_instance.name)
+        new_file = request.FILES.get('file', None)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Handle file renaming
+        if new_name and new_name != file_instance.name:
+            old_file_path = file_instance.file.path
+            new_file_path = os.path.join(os.path.dirname(old_file_path), new_name)
+            
+            # Rename the file in the filesystem
+            os.rename(old_file_path, new_file_path)
+            
+            # Update the file path in the model
+            file_instance.file.name = os.path.join(os.path.dirname(file_instance.file.name), new_name)
+            file_instance.name = new_name
 
+        # Update the file if provided
+        if new_file:
+            file_instance.file.delete(save=False)  # Delete the old file from storage
+            file_instance.file.save(new_file.name, new_file)
 
+        # Save the changes
+        file_instance.save()
+        
+        # Serialize the updated file instance
+        serializer = FileListSerializer(file_instance)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserProfileView(APIView):
-  renderer_classes = [UserRenderer]
-  permission_classes = [IsAuthenticated]
-  def get(self, request, format=None):
-    serializer = UserProfileSerializer(request.user)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+
+    # Retrieve user profile information
+    def get(self, request, format=None):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserChangePasswordView(APIView):
-  renderer_classes = [UserRenderer]
-  permission_classes = [IsAuthenticated]
-  def post(self, request, format=None):
-    serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
-    serializer.is_valid(raise_exception=True)
-    return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+
+    # Change user password
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data, context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg': 'Password Changed Successfully'}, status=status.HTTP_200_OK)
 
 class SendPasswordResetEmailView(APIView):
-  renderer_classes = [UserRenderer]
-  def post(self, request, format=None):
-    serializer = SendPasswordResetEmailSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
+    renderer_classes = [UserRenderer]
+
+    # Send password reset email
+    def post(self, request, format=None):
+        serializer = SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg': 'Password Reset link sent. Please check your Email'}, status=status.HTTP_200_OK)
 
 class UserPasswordResetView(APIView):
-  renderer_classes = [UserRenderer]
-  def post(self, request, uid, token, format=None):
-    serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
-    serializer.is_valid(raise_exception=True)
-    return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)
+    renderer_classes = [UserRenderer]
 
+    # Reset user password
+    def post(self, request, uid, token, format=None):
+        serializer = UserPasswordResetSerializer(data=request.data, context={'uid': uid, 'token': token})
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg': 'Password Reset Successfully'}, status=status.HTTP_200_OK)
